ca51591738468ef098583c75bfd3215a75349e4f
Aditya <akaditya394@gmail.com>
2022-06-20 20:29:28 +0530

    post box built

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ import PostBox from "../components/PostBox";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="my-7 max-w-5xl mx-auto">
       <Head>
         <title>Reddit Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <PostBox />
-      <div></div>
+      <div className="flex" ></div>
     </div>
   );
 };
