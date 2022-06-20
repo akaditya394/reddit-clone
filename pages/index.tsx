51019e4372df7083dd2c7a5dd372bcf5883c3b6e
Aditya <akaditya394@gmail.com>
2022-06-20 18:26:03 +0530

    avatar compoenent built

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-
+import PostBox from "../components/PostBox";
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +9,8 @@ const Home: NextPage = () => {
         <title>Reddit Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <PostBox />
+      <div></div>
     </div>
   );
 };
