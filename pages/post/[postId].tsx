d794cddc0285711934b1cd4fd7426453adb21706
Aditya <akaditya394@gmail.com>
2022-06-21 23:16:23 +0530

    comments displayed

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -7,6 +7,8 @@ import { GET_POST_BY_POST_ID } from "../../graphql/queries";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { ADD_COMMENT } from "../../graphql/mutations";
 import toast from "react-hot-toast";
+import Avatar from "../../components/Avatar";
+import ReactTimeago from "react-timeago";
 
 type FormData = {
   comment: string;
@@ -55,7 +57,6 @@ function PostPage() {
   };
 
   console.log(data);
-  
 
   return (
     <div className="mx-auto max-w-5xl my-7">
@@ -85,6 +86,33 @@ function PostPage() {
             Comment
           </button>
         </form>
+      </div>
+      <div
+        className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5
+      px-10 "
+      >
+        <hr className="py-2" />
+
+        {post?.comments.map((comment) => (
+          <div
+            className="relative flex items-center space-x-2 space-y-5"
+            key={comment.id}
+          >
+            <hr className="absolute top-10 left-7 z-0 h-16 border" />
+            <div className="z-50">
+              <Avatar seed={comment.username} />
+            </div>
+            <div className="flex flex-col">
+              <p className="py-2 text-xs text-gray-400">
+                <span className="font-semibold text-gray-600">
+                  {comment.username}
+                </span>{" "}
+                <ReactTimeago date={comment.created_at} />
+              </p>
+              <p>{comment.text}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
